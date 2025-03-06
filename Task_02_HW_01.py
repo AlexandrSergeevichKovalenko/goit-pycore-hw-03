dc3d5cc9e596868c85c5b7472b9a68f1b2ceca21
Aleksandr <akovalenko881@gmail.com>
2025-03-06 20:34:12 +0100

    task2_was corrected

--- Task_02_HW_01.py
+++ Task_02_HW_01.py
@@ -3,18 +3,20 @@ import random
 
 def get_numbers_ticket(min:int, max:int, quantity:int):
     #checking if the inputed parametrs meet the requests
-    if min >= 1 and max <= 1000 and min < quantity < max:
-        #forming the list of random values within min-max range
-        unique_numbers = random.sample(range(min, max), quantity)
-        #retun the result as list of random values
-        return sorted(unique_numbers)
-    else:
-        #return an empty list if the condition in "if" is wrong
+    if min < 1 or max > 1000 or min > max:
         return []
+    
+    if quantity > (max - min + 1):
+        return []
+
+    #forming the list of random values within min-max range
+    unique_numbers = random.sample(range(min, max+1), quantity)
+    #retun the result as list of random values
+    return sorted(unique_numbers)
 
 
 #calling the function with required arguments 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(10, 12, 11)
 #printing out a result of the function output
 print("Ваші лотерейні числа:", lottery_numbers)
 
